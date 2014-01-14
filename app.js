5df4cafb07026b52708b0f87a8dbf3fd5bdce163
kevmo <k.james.moore@gmail.com>
2014-01-13 22:19:54 -0700

    Initial app architecture complete

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
+//
+// mongo + monk
+//
+
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/nodetest');
+
 //initialize express
 
 var app = express();
@@ -43,6 +51,9 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/userlist', routes.userlist(db));
+app.get('/newuser', routes.newuser);
+app.post('/adduser', routes.adduser(db));
 //app.get('/profile', routes.profile);
 
 //
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,52 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+// database route
+
+exports.userlist = function(db){
+  return function(req,res){
+    var collection = db.get('user');
+    collection.find({}, {}, function(e, docs){
+      res.render('userlist', {
+        'user': docs
+      });
+    });
+  };
+};
+
+// add a new user to database
+
+exports.newuser = function(req, res){
+  res.render('newuser', {title: "Add New User"});
+};
+
+exports.adduser = function(db) {
+    return function(req, res) {
+
+        // Get our form values. These rely on the "name" attributes
+        var userName = req.body.username;
+        var userEmail = req.body.useremail;
+
+        // Set our collection
+        var collection = db.get('user');
+
+        // Submit to the DB
+        collection.insert({
+            "username" : userName,
+            "email" : userEmail
+        }, function (err, doc) {
+            if (err) {
+                // If it failed, return error
+                res.send("There was a problem adding the information to the database.");
+            }
+            else {
+                // If it worked, set the header so the address bar doesn't still say /adduser
+                res.location("userlist");
+                // And forward to success page
+                res.redirect("userlist");
+            }
+        });
+
+    };
+};
